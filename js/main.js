bac0cab965b354fb3b382fe422ea810feb83c993
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2023-05-08 11:11:48 +0200

    Reworked socials-commands

--- js/main.js
+++ js/main.js
@@ -2,6 +2,51 @@ const uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
 let interval = null;
 
+const github = "https://github.com/jakobfriedl"
+const linkedin = "https://www.linkedin.com/in/jakobfriedl"
+const tryhackme = "https://tryhackme.com/p/Jako0b"
+
+const commands = {
+    "help": `
+        <div class="response">
+            Available commands:<br>
+            - help<br>
+            - whoami<br> 
+            - github<br>
+            - linkedin<br>
+            - tryhackme<br> 
+            - clear
+        </div><br>`,
+    "whoami": `
+        <div class="response">
+        Hi, my name is Jakob! I am a computer science student from Austria, with a passion for cybersecurity and penetration testing.
+        </div><br>`,
+    "github": `
+        <div class="response">
+        - <a class="social" href="${github}" target="_blank">GitHub</a><br>
+        <script>
+            var win = window.open("${github}", '_blank');
+            win.focus();
+        </script>
+        </div><br>`,
+    "linkedin": `
+        <div class="response">
+        - <a class="social" href="${linkedin}" target="_blank">LinkedIn</a><br>
+        <script>
+            var win = window.open("${linkedin}", '_blank');
+            win.focus();
+        </script>
+        </div><br>`,
+    "tryhackme": `
+        <div class="response">
+        - <a class="social" href="${tryhackme}" target="_blank">TryHackMe</a><br>
+        <script>
+            var win = window.open("${tryhackme}", '_blank');
+            win.focus();
+        </script>
+        </div><br>`,
+}
+
 $(document).ready( () => {
     $("h1").trigger("mouseover");
     $(".command")
@@ -42,61 +87,28 @@ $("h1").on("mouseover", (e) => fontEffect(e, uppercase_letters));
 const ENTER = 13; 
 $(".command").on("keydown", (event) => {
     if(event.keyCode === ENTER) {
-        console.log(event)
+        // Append command & output to history
         $(".history").append(`<label><span class='username'>guest</span>@<span class='hostname'>localhost</span>:~$ ${event.currentTarget.value}</label><br>`); 
         handleCommand(event.currentTarget.value);
+        
+        // Clear input
         event.currentTarget.value = "";
+
+        // Scroll to bottom
+        $("main").scrollTop($("main")[0].scrollHeight);
     }
 }); 
 
 // Handle command 
 const handleCommand = (command) => {
-    let res = ""; 
-    switch(command) {
-        case "help":
-            res = `
-                <div class="response">
-                Available commands:<br>
-                 - help<br>
-                 - whoami<br>
-                 - socials<br>
-                 - clear
-                </div><br>`; 
-            $(".history").append(res);
-            break;
-
-        case "whoami":
-            res = `
-                <div class="response">
-                Hi, my name is Jakob! I am a computer science student from Austria, with a passion for cybersecurity and penetration testing.
-                </div><br>`
-            $(".history").append(res);
-            break;
-
-        case "socials":
-            res = `
-                <div class="response">
-                You can find me on the following platforms:<br>
-                 - <a class="social" href="https://github.com/jakobfriedl" target="_blank">GitHub</a><br>
-                 - <a class="social" href="https://www.linkedin.com/in/jakobfriedl" target="_blank">LinkedIn</a><br>
-                 - <a class="social" href="https://tryhackme.com/p/Jako0b" target="_blank">TryHackMe</a><br>
-                </div><br>`
-            $(".history").append(res);
-            break;
-
-        case "clear":
-            $(".history").html("");
-            break;
-
-        case '': 
-            break;
-
-        default:
-            res = `
-                <div class="response">
-                Command '${command}' not found
-                </div><br>`
-            $(".history").append(res);
-            break; 
+    if (command === "") return;
+    if (command === "clear") {
+        $(".history").html("");
+        return;
     }
+    if (command in commands) {
+        $(".history").append(commands[command]);
+        return;
+    }
+    $(".history").append(`<div class="response">Command '${command}' not found</div><br>`);
 }
